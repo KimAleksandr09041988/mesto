5e1be5625b341dc0f7f24c451dccad972737ce4d
Kim Aleksandr <k1mal3xandre@yandex.ru>
2022-10-23 08:52:58 +0700

    hotFix

--- js/index.js
+++ js/index.js
@@ -1,44 +1,39 @@
 const profileEditBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const popupBtnClose = document.querySelector('.popup__btn-close');
-
-profileEditBtn.addEventListener('click', () => {
-  popup.classList.add('popup_opened');
-  addValueInValue();
-})
-
-popupBtnClose.addEventListener('click', () => {
-  popup.classList.remove('popup_opened')
-})
+const popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const popupInputName = document.querySelector('.popup__input_info_name');
+const profileProfession = document.querySelector('.profile__profession');
+const popupInputProfession = document.querySelector('.popup__input_info_profession');
 
 function addValueInValue() {
-  const profileName = document.querySelector('.profile__name');
-  const popupInputName = document.querySelector('.popup__input_name');
-  const profileProfession = document.querySelector('.profile__profession');
-  const popupInputProfession = document.querySelector('.popup__input_profession');
-
   popupInputName.value = profileName.textContent;
   popupInputProfession.value = profileProfession.textContent;
 }
 
-const popupContainer = document.querySelector('.popup__container');
-
 function changeTextProfile(form) {
   form.preventDefault();
 
-  const profileName = document.querySelector('.profile__name');
-  const popupInputName = document.querySelector('.popup__input_name');
-  const profileProfession = document.querySelector('.profile__profession');
-  const popupInputProfession = document.querySelector('.popup__input_profession');
+  profileName.textContent = popupInputName.value;
+  profileProfession.textContent = popupInputProfession.value;
 
-  if (popupInputName.value) {
-    profileName.textContent = popupInputName.value;
-  }
-  if (popupInputProfession.value) {
-    profileProfession.textContent = popupInputProfession.value;
-  }
-
-  popup.classList.remove('popup_opened')
+  closePopup();
 }
 
-popupContainer.addEventListener('submit', changeTextProfile);
+function openPopup() {
+  popup.classList.add('popup_opened')
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+profileEditBtn.addEventListener('click', () => {
+  openPopup();
+  addValueInValue();
+})
+
+popupBtnClose.addEventListener('click', closePopup)
+
+popupForm.addEventListener('submit', changeTextProfile);
